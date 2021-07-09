59ed97af921f17817c3f85f2b391eac781604fd0
Alison8547 <alisonailson100@hotmail.com>
2021-07-09 18:40:27 -0300

    Estruturas-Condicionais-ELSE-IF-pt02

--- src/introducao/condicionais/Aula005EstruturasCondicionais01.java
+++ src/introducao/condicionais/Aula005EstruturasCondicionais01.java
@@ -1,11 +1,13 @@
 package introducao.condicionais;
 
-public class Aula005EstruturasCondicionais {
+public class Aula005EstruturasCondicionais01 {
     public static void main(String[] args) {
         int idade = 15;
         boolean isAutorizadoBeber = idade >= 18;
         if (isAutorizadoBeber) {
             System.out.println("Você está autorizado a beber!");
+        }else {
+            System.out.println("Você não está autorizado a beber!");
         }
         if (!isAutorizadoBeber) { // ! valor de negação.  a mesma coisa q fazer isAtorizado == false
             System.out.println("Você não está autorizado a beber!");
