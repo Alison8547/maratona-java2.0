4f7d0b098d547bc3ce60b54b7ee5f3c439d7bd74
Alison8547 <alisonailson100@hotmail.com>
2021-07-09 12:04:26 -0300

    Tipos-Primitivos-casting-pt03

--- src/introducao/Aula002TiposPrimitivos.java
+++ src/introducao/Aula002TiposPrimitivos.java
@@ -3,10 +3,10 @@ package introducao;
 public class Aula002TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, long, boolean, short
-        int idade = 10;
-        long numeroGrande = 100000;
-        double salarioDouble = 2000;
-        float salarioFloat = 2500.0F;
+        int idade = (int) 10000000000L; // cast porem tá maior que o int então ele vai colocar um número estranho
+        long numeroGrande = (long) 155.23; // como é um cast pra long ele vai ignorar as casas decimais
+        double salarioDouble = 2000.0;
+        float salarioFloat = (float) 2500.0D; // cast normal sem passar os limites então ele vai colocar o número certo
         byte idadeByte = -128;
         short idadeShort = 32000;
         boolean verdadeiro = true;
@@ -15,6 +15,9 @@ public class Aula002TiposPrimitivos {
         System.out.println("Sua idade é: " + idade + " anos");
         System.out.println(verdadeiro);
         System.out.println("Char: " + caractere);
+        System.out.println(idade);
+        System.out.println(salarioFloat);
+        System.out.println(numeroGrande);
 
     }
 }
