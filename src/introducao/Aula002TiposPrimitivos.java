10017c5feca0e824031496d73c83d17972b20219
Alison8547 <alisonailson100@hotmail.com>
2021-07-09 11:48:32 -0300

    Tipos-Primitivos-pt-01-02

---
+++ src/introducao/Aula002TiposPrimitivos.java
@@ -0,0 +1,20 @@
+package introducao;
+
+public class Aula002TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, long, boolean, short
+        int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500.0F;
+        byte idadeByte = -128;
+        short idadeShort = 32000;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+        System.out.println("Sua idade é: " + idade + " anos");
+        System.out.println(verdadeiro);
+        System.out.println("Char: " + caractere);
+
+    }
+}
