63b61f37022ff05bc49d0d8046639b86b6c089c3
Alison8547 <alisonailson100@hotmail.com>
2021-07-12 16:48:14 -0300

    Arrays-Multidimensionais-Foreach-pt02

--- src/introducao/arrays/ArraysMultidimensionais04.java
+++ src/introducao/arrays/ArraysMultidimensionais04.java
@@ -13,8 +13,16 @@ public class ArraysMultidimensionais04 {
         dias[1][2] = 15;
 
         for (int i = 0; i < dias.length; i++) {
-            for (int j = 0; j < dias[0].length; j++) {
+            for (int j = 0; j < dias[i].length; j++) {
                 System.out.println(dias[i][j]);
+            }
+        }
+
+        System.out.println();
+
+        for (int[] arrayBase : dias) {
+            for (int num : arrayBase) {
+                System.out.println(num);
             }
         }
     }
