562895b291f61c7a2863c533dba0c9f8436cc7a3
Alison8547 <alisonailson100@hotmail.com>
2021-07-13 11:20:38 -0300

    Orientação-Objetos-Métodos-pt01

---
+++ src/javacore/introducaometodos/dominio/Calculadora.java
@@ -0,0 +1,12 @@
+package javacore.introducaometodos.dominio;
+
+public class Calculadora {
+
+    public void somaDoisNumeros() {
+        System.out.println(10 + 10);
+    }
+
+    public void subtraiDoisNumeros() {
+        System.out.println(15 - 5);
+    }
+}
