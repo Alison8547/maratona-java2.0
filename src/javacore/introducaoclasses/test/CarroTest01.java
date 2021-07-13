84a4baafa9cbccd71087aa230525619d2ea94b9f
Alison8547 <alisonailson100@hotmail.com>
2021-07-13 10:58:22 -0300

    Orientação-Objetos-exercício-classes

---
+++ src/javacore/introducaoclasses/test/CarroTest01.java
@@ -0,0 +1,28 @@
+package javacore.introducaoclasses.test;
+
+import javacore.introducaoclasses.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+        Carro carro2 = new Carro();
+
+        carro1.nome = "GTR";
+        carro1.modelo = "Nissan";
+        carro1.ano = 2014;
+
+        carro2.nome = "Gol";
+        carro2.modelo = "Volkswagen";
+        carro2.ano = 2000;
+
+        System.out.println("Carro1");
+        System.out.println(carro1.nome);
+        System.out.println(carro1.modelo);
+        System.out.println(carro1.ano);
+
+        System.out.println("\nCarro2");
+        System.out.println(carro2.nome);
+        System.out.println(carro2.modelo);
+        System.out.println(carro2.ano);
+    }
+}
