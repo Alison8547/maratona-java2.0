c29cf3af27d84de1f9f6913aae93f9d746f2fc65
Alison8547 <alisonailson100@hotmail.com>
2021-07-13 19:26:12 -0300

    Orientação-Objetos-Métodos-pt03-retorno-pt01

---
+++ src/javacore/introducaometodos/test/CalculadoraTest03.java
@@ -0,0 +1,12 @@
+package javacore.introducaometodos.test;
+
+import javacore.introducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest03 {
+    public static void main(String[] args) {
+
+        Calculadora calc = new Calculadora();
+        double result = calc.divideDoisNumeros(20, 0);
+        System.out.println(result);
+    }
+}
