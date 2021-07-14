ea1fbdc64252bd81a4f9ca6a8248b09399932f9a
Alison8547 <alisonailson100@hotmail.com>
2021-07-14 14:23:32 -0300

    Orientação-Objetos-Métodos-pt04-retorno-pt02

--- src/javacore/introducaometodos/dominio/Calculadora.java
+++ src/javacore/introducaometodos/dominio/Calculadora.java
@@ -21,4 +21,12 @@ public class Calculadora {
         }
         return num1 / num2;
     }
+
+    public void imprimeDivisaoDeDoisNumeros(double num1, double num2) {
+        if (num2 == 0) {
+            System.out.println("Não existe divisão por zero");
+            return; // funciona como um break
+        }
+        System.out.println(num1 / num2);
+    }
 }
--- src/javacore/introducaometodos/test/CalculadoraTest03.java
+++ src/javacore/introducaometodos/test/CalculadoraTest03.java
@@ -8,5 +8,6 @@ public class CalculadoraTest03 {
         Calculadora calc = new Calculadora();
         double result = calc.divideDoisNumeros(20, 0);
         System.out.println(result);
+        calc.imprimeDivisaoDeDoisNumeros(25, 0);
     }
 }
