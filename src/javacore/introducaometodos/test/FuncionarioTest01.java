067cca17905e86129ff8e24cfb0fca926f7e3f18
Alison8547 <alisonailson100@hotmail.com>
2021-07-16 10:52:44 -0300

    Orientação-Objetos-Modificador-de-acesso-private-get-e-set-pt03

--- src/javacore/introducaometodos/test/FuncionarioTest01.java
+++ src/javacore/introducaometodos/test/FuncionarioTest01.java
@@ -5,9 +5,10 @@ import javacore.introducaometodos.dominio.Funcionario;
 public class FuncionarioTest01 {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
-        funcionario.nome = "Tiago";
-        funcionario.idade = 20;
-        funcionario.salarios = new double[]{1200, 900, 845.45};
+        funcionario.setNome("Tiago");
+        funcionario.setIdade(20);
+        funcionario.setSalarios(new double[]{1200, 900, 845.45});
         funcionario.imprime();
+        System.out.println("Média: "+funcionario.getMedia());
     }
 }
