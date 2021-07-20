6135bc5cca6ac4e3cf62389ec1a39a021b41863e
Alison8547 <alisonailson100@hotmail.com>
2021-07-20 17:31:32 -0300

    Orientação-Objetos-Herança-pt01

---
+++ src/javacore/heranca/test/HerancaTest01.java
@@ -0,0 +1,18 @@
+package javacore.heranca.test;
+
+import javacore.heranca.dominio.Endereco;
+import javacore.heranca.dominio.Funcionario;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua da liberdade");
+        endereco.setCep("111111-111");
+        funcionario.setNome("Alison");
+        funcionario.setCpf("705.789.151.45");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(2000.0);
+        funcionario.imprime();
+    }
+}
