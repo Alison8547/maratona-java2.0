e68b64143baf0ac78e067abe8ac1daacd8b41271
Alison8547 <alisonailson100@hotmail.com>
2021-07-24 12:14:31 -0300

    Orientação-Objetos-Modificador-final-pt03-Classes-e-métodos

--- src/javacore/modificadorfinal/dominio/Carro.java
+++ src/javacore/modificadorfinal/dominio/Carro.java
@@ -1,12 +1,15 @@
 package javacore.modificadorfinal.dominio;
 
-public class Carro {
+public final class Carro { // final na class ela não pode ser extends
     private String nome;
     public static final double VELOCIDADE_MAXIMA = 250; // final nunca pode ser mudado.
     public final Comprador COMPRADOR = new Comprador();
 
     public Comprador getCOMPRADOR() {
         return COMPRADOR;
+    }
+    public final void imprime(){  // Mesma coisa no método se colocar final ela não pode ser sobrescrita/alterado
+        System.out.println(this.nome);
     }
 
     //    static { Podia iniciar aq pq é static e inicializa direto
