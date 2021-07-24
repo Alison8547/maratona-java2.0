9bc73136688333eb333db4c7e6e5502eda8817f2
Alison8547 <alisonailson100@hotmail.com>
2021-07-24 11:20:20 -0300

    Orientação-Objetos-Sobrescrita-do-método-toString

---
+++ src/javacore/sobrescrita/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package javacore.sobrescrita.test;
+
+import javacore.sobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dragon ball Z");
+        System.out.println(anime);
+    }
+}
