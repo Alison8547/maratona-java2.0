ce34c0f7005f902486c8c7bf209739332a654fd6
Alison8547 <alisonailson100@hotmail.com>
2021-07-28 16:52:18 -0300

    Orientação-Objetos-Enumeração-pt02-Construtores-e-atributos

--- src/javacore/enumeracao/test/ClienteTest01.java
+++ src/javacore/enumeracao/test/ClienteTest01.java
@@ -3,16 +3,14 @@ package javacore.enumeracao.test;
 import javacore.enumeracao.dominio.Cliente;
 import javacore.enumeracao.dominio.TipoCliente;
 
+import static javacore.enumeracao.dominio.Cliente.*;
+
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Alison", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Diego", TipoCliente.PESSOA_FISICA);
-        Cliente cliente3 = new Cliente("Juliana", TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("Maria", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Alison", TipoCliente.PESSOA_FISICA, TipoPagamento.CREDITO);
+        Cliente cliente2 = new Cliente("Maria", TipoCliente.PESSOA_JURIDICA, TipoPagamento.DEBITO);
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
 
     }
 }
