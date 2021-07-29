2f8f97af254c3fa2a0f300b90e31d3df266cd43d
Alison8547 <alisonailson100@hotmail.com>
2021-07-29 11:24:07 -0300

    Orientação-Objetos-Interfaces-pt02-Implementando-múltiplas-interfaces

--- src/javacore/interfaces/dominio/DataLoader.java
+++ src/javacore/interfaces/dominio/DataLoader.java
@@ -2,4 +2,8 @@ package javacore.interfaces.dominio;
 
 public interface DataLoader {
     void load();
+
+    default void checkPermission() {  // Com default a partir do java 8 agora podemos criar um método com corpo e não dar o erro porque implementa o método se quiser
+        System.out.println("Fazendo checagem de permissões");
+    }
 }
--- src/javacore/interfaces/dominio/DatabaseLoader.java
+++ src/javacore/interfaces/dominio/DatabaseLoader.java
@@ -1,8 +1,18 @@
 package javacore.interfaces.dominio;
 
-public class DatabaseLoader implements DataLoader {
+public class DatabaseLoader implements DataLoader, DataRemover { // implements pode implementar várias.
     @Override
     public void load() {
         System.out.println("Carregando dados de um banco de dados");
+    }
+
+    @Override
+    public void remove() {
+        System.out.println("Removendo dados do banco de dados");
+    }
+
+    @Override
+    public void checkPermission() { // Mas se eu quiser mudar eu posso, pra ser específico do banco de dados
+        System.out.println("Fazendo checagem de permissões banco de dados");
     }
 }
