c9fc5c2de494c178f4a983f3f921a3d0af8d7bbc
Alison8547 <alisonailson100@hotmail.com>
2021-08-08 12:18:10 -0300

    Classes-Utilitárias-Wrappers-pt02

--- src/javacore/wrapper/test/WrapperTest01.java
+++ src/javacore/wrapper/test/WrapperTest01.java
@@ -13,11 +13,26 @@ public class WrapperTest01 {
 
         Byte byteW = 1;
         Integer intW = 457;
-        Double doubleW = 15d;
+        Double doubleW = 15d;  // Autoboxing
         Float floatW = 544f;
         Long longW = 4564L;
         Short shortW = 12;
         Character charW = 'A';
         Boolean booleanW = true;
+
+        int i = intW; // boxing
+        Integer int2W = Integer.parseInt("15");
+        System.out.println(int2W);
+        Boolean verdadeiro = Boolean.parseBoolean(null);
+        System.out.println(verdadeiro);
+
+        System.out.println(Character.isDigit('A'));
+        System.out.println(Character.isLetterOrDigit('1'));
+        System.out.println(Character.isLowerCase('a'));
+        System.out.println(Character.isUpperCase('S'));
+        System.out.println(Character.toUpperCase('b'));
+        System.out.println(Character.toLowerCase('G'));
+
+
     }
 }
