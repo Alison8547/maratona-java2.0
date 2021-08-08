dfb1c1ee03d7cb9f5af7ed9f1c6e8520d1d7b5f7
Alison8547 <alisonailson100@hotmail.com>
2021-08-08 12:04:02 -0300

    Classes-Utilitárias-Wrappers-pt01

---
+++ src/javacore/wrapper/test/WrapperTest01.java
@@ -0,0 +1,23 @@
+package javacore.wrapper.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        int intP = 457;
+        double doubleP = 15d;
+        float floatP = 544f;
+        long longP = 4564L;
+        short shortP = 12;
+        char charP = 'A';
+        boolean booleanP = true;
+
+        Byte byteW = 1;
+        Integer intW = 457;
+        Double doubleW = 15d;
+        Float floatW = 544f;
+        Long longW = 4564L;
+        Short shortW = 12;
+        Character charW = 'A';
+        Boolean booleanW = true;
+    }
+}
