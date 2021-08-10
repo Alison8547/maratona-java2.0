5fe752e220863b5df37aebe3c10e4ab0a969f96b
Alison8547 <alisonailson100@hotmail.com>
2021-08-10 14:51:27 -0300

    Classes-Utilitárias-Internacionalização-de-moeda-com-Locale

--- src/javacore/formatacao/NumberFormatTest02.java
+++ src/javacore/formatacao/NumberFormatTest02.java
@@ -4,27 +4,28 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
-public class NumberFormatTest01 {
+public class NumberFormatTest02 {
     public static void main(String[] args) {
         Locale localeEUA = Locale.US;
         Locale localeJapao = Locale.JAPAN;
         Locale localeItaly = Locale.ITALY;
         NumberFormat[] numberFormats = new NumberFormat[4];
-        numberFormats[0] = NumberFormat.getInstance();
-        numberFormats[1] = NumberFormat.getInstance(localeItaly);
-        numberFormats[2] = NumberFormat.getInstance(localeEUA);
-        numberFormats[3] = NumberFormat.getInstance(localeJapao);
+        numberFormats[0] = NumberFormat.getCurrencyInstance();
+        numberFormats[1] = NumberFormat.getCurrencyInstance(localeItaly);
+        numberFormats[2] = NumberFormat.getCurrencyInstance(localeEUA);
+        numberFormats[3] = NumberFormat.getCurrencyInstance(localeJapao);
 
-        double valor = 100_000.4654;
+        double valor = 100_000_000.4654;
 
         for (NumberFormat nf : numberFormats) {
+            System.out.println(nf.getMaximumFractionDigits());
             System.out.println(nf.format(valor));
         }
 
-        String valorString = "1000.230";
+        String valorString = "$1000.230";
 
         try {
-            System.out.println(numberFormats[0].parse(valorString));
+            System.out.println(numberFormats[2].parse(valorString));
         } catch (ParseException e) {
             e.printStackTrace();
         }
