67924df1d967873efac195f7a0c210176143bcaa
Alison8547 <alisonailson100@hotmail.com>
2021-08-14 11:21:47 -0300

    Classes-Utilitárias-IO-pt03-FileReader.

--- src/javacore/io/test/FileReaderTest01.java
+++ src/javacore/io/test/FileReaderTest01.java
@@ -1,2 +1,20 @@
-package javacore.io.test;public class FileReaderTest01 {
+package javacore.io.test;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderTest01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try (FileReader fr = new FileReader(file)) {
+            int i;
+            while ((i = fr.read()) != -1) {
+                System.out.print((char) i);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
