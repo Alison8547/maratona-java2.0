053550b5ac4655b23038b363ab001cc018118545
Alison8547 <alisonailson100@hotmail.com>
2021-08-14 11:38:39 -0300

    Classes-Utilitárias-IO-pt05-BufferedReader

--- src/javacore/io/test/BufferedWriterTest01.java
+++ src/javacore/io/test/BufferedWriterTest01.java
@@ -9,10 +9,10 @@ public class BufferedWriterTest01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try (FileWriter fw = new FileWriter(file, true);
-             BufferedWriter br = new BufferedWriter(fw)) {
-            br.write("Curso Dev dojo é muito bom");
-            br.newLine();
-            br.flush();
+             BufferedWriter bw = new BufferedWriter(fw)) {
+            bw.write("Curso Dev dojo é muito bom");
+            bw.newLine();
+            bw.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
