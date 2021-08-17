6f9f8e5987cf891d03e3564594dce3ec6c9d2bef
Alison8547 <alisonailson100@hotmail.com>
2021-08-16 21:48:28 -0300

    Classes-Utilitárias-NIO-pt15-ZipOutputStream

---
+++ src/javacore/nio/test/ZipOutputStreamTest01.java
@@ -0,0 +1,36 @@
+package javacore.nio.test;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class ZipOutputStreamTest01 {
+    public static void main(String[] args) {
+        Path arquivoZip = Paths.get("pasta2/arquivo.zip");
+        Path arquivosParaZipar = Paths.get("pasta/subpasta/subsubpasta");
+        zip(arquivoZip, arquivosParaZipar);
+
+    }
+
+    private static void zip(Path arquivoZip, Path arquivoParaZipar) {
+        try (ZipOutputStream streamZip = new ZipOutputStream(Files.newOutputStream(arquivoZip));
+             DirectoryStream<Path> directoryStream = Files.newDirectoryStream(arquivoParaZipar)) {
+
+            for (Path file : directoryStream) {
+                ZipEntry zipEntry = new ZipEntry(file.getFileName().toString());
+                streamZip.putNextEntry(zipEntry);
+                Files.copy(file, streamZip);
+                streamZip.closeEntry();
+            }
+            System.out.println("Arquivo criado com sucesso!");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
