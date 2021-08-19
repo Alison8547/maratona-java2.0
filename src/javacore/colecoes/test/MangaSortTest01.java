64d538908f62f76db140fcc2b37a6883f8d5085c
Alison8547 <alisonailson100@hotmail.com>
2021-08-19 20:35:40 -0300

    Coleções-pt11-Sorting-lists-pt03-Comparator

--- src/javacore/colecoes/test/MangaSortTest01.java
+++ src/javacore/colecoes/test/MangaSortTest01.java
@@ -4,16 +4,24 @@ import javacore.colecoes.dominio.Manga;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+class MangaByIdComparator implements Comparator<Manga> {
+    @Override
+    public int compare(Manga manga1, Manga manga2) {
+        return manga1.getId().compareTo(manga2.getId());
+    }
+}
 
 public class MangaSortTest01 {
     public static void main(String[] args) {
         List<Manga> mangas = new ArrayList<>();
-        mangas.add( new Manga(5L,"Naruto",19.90));
-        mangas.add( new Manga(1L,"One piece",9.90));
-        mangas.add( new Manga(4L,"Pokemon",5.50));
-        mangas.add( new Manga(3L,"Naruto",25.50));
-        mangas.add( new Manga(2L,"Death note",15.50));
+        mangas.add(new Manga(5L, "Naruto", 19.90));
+        mangas.add(new Manga(1L, "One piece", 9.90));
+        mangas.add(new Manga(4L, "Pokemon", 5.50));
+        mangas.add(new Manga(3L, "Naruto", 25.50));
+        mangas.add(new Manga(2L, "Death note", 15.50));
         for (Manga manga : mangas) {
             System.out.println(manga);
         }
@@ -22,7 +30,12 @@ public class MangaSortTest01 {
         for (Manga manga : mangas) {
             System.out.println(manga);
         }
-
+        System.out.println("-------------------");
+      //  Collections.sort(mangas, new MangaByIdComparator());
+        mangas.sort(new MangaByIdComparator()); // ? É um.. passando com comparator para ser organizado pelo id
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
 
     }
 }
