bb7e6086eb757d4bb10fbc592f12f8e1dd4729c2
Alison8547 <alisonailson100@hotmail.com>
2021-08-21 13:20:31 -0300

    Generics-pt03-Wildcard-pt02

--- src/javacore/generics/test/WildcardTest02.java
+++ src/javacore/generics/test/WildcardTest02.java
@@ -1,22 +1,31 @@
 package javacore.generics.test;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WildcardTest02 {
     public static void main(String[] args) {
         List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
         List<Gato> gatos = List.of(new Gato(), new Gato());
+        printConsulta(cachorros);
+        printConsulta(gatos);
+        List<Animal> animals = new ArrayList<>();
+        printColsultaAnimal(animals);
 
     }
-        // Type erasure
-    public static void printConsulta(List<Animal> animals) {
+
+    // Type erasure
+    public static void printConsulta(List<? extends Animal> animals) { // Aqui tô falando que posso passar animal ou qualquer um que seja filho
         for (Animal animal : animals) {
             animal.consulta();
         }
 
-        animals.add(new Cachorro());
+    }
 
+    public static void printColsultaAnimal(List<? super Animal> animals) { // Aqui qualquer um que seja pai
+        animals.add(new Cachorro());
+        animals.add(new Gato());
     }
 }
 
