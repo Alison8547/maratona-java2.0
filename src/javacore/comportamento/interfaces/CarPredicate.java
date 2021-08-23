98b05c16f567d3e9cc559ede48a21b20a06f6615
Alison8547 <alisonailson100@hotmail.com>
2021-08-23 14:55:43 -0300

    Lambdas-pt01-Predicate

--- src/javacore/comportamento/interfaces/CarPredicate.java
+++ src/javacore/comportamento/interfaces/CarPredicate.java
@@ -2,6 +2,10 @@ package javacore.comportamento.interfaces;
 
 import javacore.comportamento.dominio.Car;
 
+@FunctionalInterface
 public interface CarPredicate {
-   boolean test(Car car);
+   // anonimas, funções, conciso
+   boolean test(Car car);  // funções lambdas tem q ter um interface e metodo abstrato.. boolean o tipo q ela vai retornar nesse exemplo
+   //(parametro) -> <expressão>
+   //(Car car) -> car.getColor().equals("green");
 }
