eb5754f586371f822556f901960e907065cab0d7
Alison8547 <alisonailson100@hotmail.com>
2021-08-24 22:58:12 -0300

    Method-Reference-pt02-Referência-a-métodos-não-estáticos

--- src/javacore/lambdas/test/MethodReferenceTest02.java
+++ src/javacore/lambdas/test/MethodReferenceTest02.java
@@ -5,13 +5,13 @@ import javacore.lambdas.service.AnimeComparators;
 
 import java.util.ArrayList;
 import java.util.List;
-// Reference to a static method
-public class MethodReferenceTest01 {
+//Reference to an instance method of a particular object
+public class MethodReferenceTest02 {
     public static void main(String[] args) {
+        AnimeComparators animeComparators = new AnimeComparators();
         List<Anime> animeList = new ArrayList<>(List.of(new Anime("One piece", 987), new Anime("HunterxHunter", 112), new Anime("Naruto", 220), new Anime("Death note", 50)));
-        //   animeList.sort((a1, a2) -> a1.getTitle().compareTo(a2.getTitle()));
-        //   animeList.sort(AnimeComparators::compareByTitle);
-        animeList.sort(AnimeComparators::compareByEpisode);
+        animeList.sort(animeComparators::compareByEpisodeNonStatic);
+        // animeList.sort((a1,a2) -> animeComparators.compareByEpisodeNonStatic(a1,a2));
         System.out.println(animeList);
     }
 }
