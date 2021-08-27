9d6d999148df0912c8c80aab5af0c4b5c4b63c0e
Alison8547 <alisonailson100@hotmail.com>
2021-08-27 10:23:50 -0300

    Streams-pt12-Collectors-pt02-Grouping-by-pt01

---
+++ src/javacore/streams/test/StreamTest12.java
@@ -0,0 +1,52 @@
+package javacore.streams.test;
+
+import javacore.streams.dominio.Category;
+import javacore.streams.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StreamTest12 {
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 8.99, Category.FANTASY),
+            new LightNovel("Overlord", 3.99, Category.FANTASY),
+            new LightNovel("Violet Evergarden", 5.99, Category.DRAMA),
+            new LightNovel("No Game no life", 2.99, Category.FANTASY),
+            new LightNovel("Fullmetal Alchemist", 5.99, Category.FANTASY),
+            new LightNovel("Kumo desuga", 1.99, Category.FANTASY),
+            new LightNovel("Kumo desuga", 1.99, Category.FANTASY),
+            new LightNovel("Monogatari", 4.00, Category.ROMANCE)
+    ));
+
+    public static void main(String[] args) {
+        Map<Category, List<LightNovel>> categoryListMap = new HashMap<>();
+        List<LightNovel> fantasy = new ArrayList<>();
+        List<LightNovel> drama = new ArrayList<>();
+        List<LightNovel> romance = new ArrayList<>();
+
+        for (LightNovel lightNovel : lightNovels) {
+            switch (lightNovel.getCategory()) {
+                case FANTASY:
+                    fantasy.add(lightNovel);
+                    break;
+                case DRAMA:
+                    drama.add(lightNovel);
+                    break;
+                case ROMANCE:
+                    romance.add(lightNovel);
+                    break;
+            }
+        }
+        categoryListMap.put(Category.FANTASY, fantasy);
+        categoryListMap.put(Category.DRAMA, drama);
+        categoryListMap.put(Category.ROMANCE, romance);
+        System.out.println(categoryListMap);
+
+        // Isso tudo de codigo acima, apenas usando stream só foi uma linha.
+        final Map<Category, List<LightNovel>> collect = lightNovels.stream().collect(Collectors.groupingBy(LightNovel::getCategory));
+        System.out.println(collect);
+    }
+}
