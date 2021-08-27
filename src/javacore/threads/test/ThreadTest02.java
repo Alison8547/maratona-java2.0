b65cb2fcf7abceff4527c0696b2796244286ed68
Alison8547 <alisonailson100@hotmail.com>
2021-08-27 16:54:33 -0300

    Threads-pt04-Yield-e-Join

---
+++ src/javacore/threads/test/ThreadTest02.java
@@ -0,0 +1,38 @@
+package javacore.threads.test;
+
+
+class ThreadExampleRunnable2 implements Runnable {
+    private String c;
+
+    public ThreadExampleRunnable2(String c) {
+        this.c = c;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        for (int i = 0; i < 500; i++) {
+            System.out.print(c);
+            if (i % 100 == 0) {
+                System.out.println();
+            }
+          //  Thread.yield();
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class ThreadTest02 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new ThreadExampleRunnable2("KA"));
+        Thread t2 = new Thread(new ThreadExampleRunnable2("ME"));
+        t1.setPriority(Thread.MAX_PRIORITY);
+        t1.start();
+        t1.join(); // join. dizendo pra thread main q ela só vai executar o outro quando terminar a thread t1
+        t2.start();
+    }
+}
