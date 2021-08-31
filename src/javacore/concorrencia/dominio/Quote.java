e31b7b906a8ffa82e541513adf7b15d2bdde6418
Alison8547 <alisonailson100@hotmail.com>
2021-08-31 17:51:34 -0300

    Concorrência-pt17-CompletableFuture-pt06-allOf-anyOf

--- src/javacore/concorrencia/dominio/Quote.java
+++ src/javacore/concorrencia/dominio/Quote.java
@@ -1,6 +1,6 @@
 package javacore.concorrencia.dominio;
 
-public class Quote {
+public final class Quote {
     private final String store;
     private final double price;
     private final Discount.Code discountCode;
