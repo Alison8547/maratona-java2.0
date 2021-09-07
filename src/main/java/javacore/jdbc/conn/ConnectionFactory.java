55a1c02dfc916f8dd87309bbef3de1fba20811d7
Alison8547 <alisonailson100@hotmail.com>
2021-09-07 14:21:02 -0300

    JDBC-pt05-Adicionando-dependência-e-conectando-com-banco

--- src/main/java/javacore/jdbc/conn/ConnectionFactory.java
+++ src/main/java/javacore/jdbc/conn/ConnectionFactory.java
@@ -1,5 +1,23 @@
 package javacore.jdbc.conn;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
+    public static Connection getConnection() {
+        //java.sql = Connection, Statement, ResultSet, DriverManager
+        String url = "jdbc:mysql://localhost:3306/anime_store";
+        String username = "root";
+        String password = "root";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
---
+++ src/main/java/javacore/jdbc/test/ConnectionFactoryTest.java
@@ -0,0 +1,9 @@
+package javacore.jdbc.test;
+
+import javacore.jdbc.conn.ConnectionFactory;
+
+public class ConnectionFactoryTest {
+    public static void main(String[] args) {
+        ConnectionFactory.getConnection();
+    }
+}
