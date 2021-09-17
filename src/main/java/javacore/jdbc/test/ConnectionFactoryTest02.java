06dcc679b24c713b75ecfaaa66f3a6d56766bdc5
Alison8547 <alisonailson100@hotmail.com>
2021-09-17 17:45:54 -0300

    JDBC-pt20-Connected-RowSet-JdbcRowSet-pt01

---
+++ src/main/java/javacore/jdbc/test/ConnectionFactoryTest02.java
@@ -0,0 +1,15 @@
+package javacore.jdbc.test;
+
+import javacore.jdbc.dominio.Producer;
+import javacore.jdbc.service.ProducerServiceRowSet;
+import lombok.extern.log4j.Log4j2;
+
+import java.util.List;
+
+@Log4j2
+public class ConnectionFactoryTest02 {
+    public static void main(String[] args) {
+        List<Producer> producers = ProducerServiceRowSet.findByNameJdbcRowSet("NHK");
+        log.info(producers);
+    }
+}
