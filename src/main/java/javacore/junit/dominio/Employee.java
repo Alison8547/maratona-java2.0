379681c6445bddc871154e99bffc01bb26c28088
Alison8547 <alisonailson100@hotmail.com>
2021-09-18 22:25:30 -0300

    Pattern-Matching-for-instanceof

---
+++ src/main/java/javacore/junit/dominio/Employee.java
@@ -0,0 +1,13 @@
+package javacore.junit.dominio;
+
+public class Employee {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public Employee(String id) {
+        this.id = id;
+    }
+}
